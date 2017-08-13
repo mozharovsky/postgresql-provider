c00b18952fb4d2cf9e6e10bdd6ccae6d86ea6ecd
Eugene Mozharovsky <mozharovsky@live.com>
2017-08-13 20:56:38 +0300

    Using local `fluent-provider` repo

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         // PostgreSQL driver for Fluent
         .Package(url: "https://github.com/vapor/postgresql-driver.git", majorVersion: 2),
         // A provider for including Fluent in Vapor applications
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/mozharovsky/fluent-provider.git", majorVersion: 1),
         // A web framework and server for Swift that works on macOS and Ubuntu.
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
